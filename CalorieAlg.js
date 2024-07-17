c8e25f972a6e01e996d83f01417f9e554e1cdd99
Brock <owensbrock26@gmail.com>
2024-07-16 20:49:15 -0700

    Version 1.0.7 fix for 1.0.6 crashing

--- CalorieAlg.js
+++ CalorieAlg.js
@@ -8,3 +8,71 @@ const calculateNutrition = (totalCalories) => {
   };
   
   export default calculateNutrition;
+
+
+
+  /*
+
+
+{
+  "expo": {
+    "name": "TrimFit",
+    "slug": "TrimFitFinal",
+    "version": "1.0.7",
+    "orientation": "portrait",
+    "icon": "./assets/images/FinalAppIcon.png",
+    "scheme": "myapp",
+    "userInterfaceStyle": "automatic",
+    "splash": {
+      "image": "./assets/images/FinalAppIcon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#232526"
+    },
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.owensbrcok26.TrimFitProduction"
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/FinalAppIcon.png",
+        "backgroundColor": "#232526"
+      },
+      "permissions": [
+        "android.permission.CAMERA"
+      ],
+      "package": "com.owensbrcok26.TrimFitProduction"
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      [
+        "expo-barcode-scanner",
+        {
+          "cameraPermission": "Allow $(PRODUCT_NAME) to access your camera to scan food barcodes check our privacy policy for more information",
+          "microphonePermission": "Allow $(PRODUCT_NAME) to access your microphone",
+          "recordAudioAndroid": true
+        }
+      ],
+      "react-native-google-mobile-ads"
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    "extra": {
+      "eas": {
+        "projectId": "9f640de3-5467-46f3-9fee-4d74a7cf79e5"
+      }
+    }
+  },
+  "react-native-google-mobile-ads": {
+    "android_app_id": "ca-app-pub-1107416661606980~9855155085",
+    "ios_app_id": "ca-app-pub-1107416661606980~7807576241"
+  }
+}
+
+
+
+  */
